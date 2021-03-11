f3653d1966805d6e90e24d56c9d35aeb0c88fed8
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-03-11 02:16:45 -0500

    simple structs and CLI arguments

---
+++ farey-sequence/farey.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var args = std.process.args();
+    _ = args.skip();
+    const arg1 = try args.next(std.testing.allocator) orelse {
+        std.debug.print("Usage: ./farey.exe <max_denominator>\n", .{});
+        return;
+    };
+    const d_max = std.fmt.parseUnsigned(u32, arg1, 10) catch |e| {
+        std.debug.print("invalid value for max_denominator: {s}\n", .{arg1});
+        return;
+    };
+    explore(Frac{ .n = 0, .d = 1 }, Frac{ .n = 1, .d = 1 }, d_max);
+}
+
+const Frac = struct {
+    n: u32,
+    d: u32,
+};
+
+fn explore(lo: Frac, hi: Frac, d_max: u32) void {
+    const mid = find_midpoint(lo, hi);
+    if (mid.d > d_max) return;
+
+    explore(lo, mid, d_max);
+    std.debug.print("{d:.4} --- {}/{}\n", .{ @intToFloat(f64, mid.n) / @intToFloat(f64, mid.d), mid.n, mid.d });
+    explore(mid, hi, d_max);
+}
+
+fn find_midpoint(lo: Frac, hi: Frac) Frac {
+    return Frac{ .n = lo.n + hi.n, .d = lo.d + hi.d };
+}
